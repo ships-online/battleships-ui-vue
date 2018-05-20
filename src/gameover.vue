279fe56af7b829e38e7ae283552525dc02f43925
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-05-20 15:17:56 +0200

    Added GameOver view.

---
+++ src/gameover.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1>Battleships</h1>
+		<h2>Game over</h2>
+
+		<p v-if="error === 'not-exist'">Game does not exist.</p>
+		<p v-else-if="error === 'not-available'">Game has already started.</p>
+		<p v-else-if="error === 'host-left'">Your opponent has left the game.</p>
+		<p v-else-if="error === 'opponent-left'">Your opponent has left the game.</p>
+		<p v-else>{{ error }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		computed: {
+			error() {
+				return this.$parent.error.toString();
+			}
+		}
+	};
+</script>
