0af7825476029da419e5b776d5d130f6bc57dda6
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-06-23 16:38:15 +0200

    Improved docs.

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,31 @@
-export function getCellSize( size, clientWidth ) {
-	return Math.min( Math.floor( clientWidth / ( ( size * 2 ) + 1 ) ), 40 );
+/**
+ * Returns optimal size for single cell of the battlefield.
+ *
+ * @param {Number} size Battleship size.
+ * @param {Number} availableWidth Available size for two battlefields.
+ * @returns {number}
+ */
+export function getCellSize( size, availableWidth ) {
+	return Math.min( Math.floor( availableWidth / ( ( size * 2 ) + 1 ) ), 40 );
 }
 
+/**
+ * Add `px` to the given value.
+ *
+ * @param {*} value
+ * @returns {String}
+ */
 export function toPx( value ) {
 	return `${ value }px`;
 }
 
+/**
+ * Converts Collection to Array and bind this array with collection
+ * to be in the same state after adding or removing items.
+ *
+ * @param {Collection} collection
+ * @returns {Array}
+ */
 export function collectionToArray( collection ) {
 	const result = Array.from( collection );
 
@@ -15,11 +35,27 @@ export function collectionToArray( collection ) {
 	return result;
 }
 
+/**
+ * Settings stored in the browser storage.
+ *
+ * @private
+ */
 class Settings {
+	/**
+	 * @param {LocalStorage} storage
+	 */
 	constructor( storage ) {
+		/**
+		 * @private
+		 * @type {LocalStorage}
+		 */
 		this._storage = storage;
 	}
 
+	/**
+	 * @param {String} key
+	 * @param {*} value
+	 */
 	set( key, value ) {
 		const data = this.get();
 
@@ -28,6 +64,12 @@ class Settings {
 		this._storage.setItem( 'battleships-game', JSON.stringify( data ) );
 	}
 
+	/**
+	 * Returns data stored behind the given key or the entire data object when key is not defined.
+	 *
+	 * @param {String} [key]
+	 * @returns {*}
+	 */
 	get( key ) {
 		let data = this._storage.getItem( 'battleships-game' );
 
@@ -45,4 +87,9 @@ class Settings {
 	}
 }
 
+/**
+ * Instance for stores and gets any data that needs to be stored in the browser storage.
+ *
+ * @type {Settings}
+ */
 export const settings = new Settings( window.localStorage );
